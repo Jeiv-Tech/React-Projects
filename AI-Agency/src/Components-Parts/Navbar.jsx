7f7976aa686fb8457418fc413e874177f2f8aceb
Jeiv-Tech <jeivjeiv85@gmail.com>
2026-09-22 23:19:40 +0800

    assets

--- AI-Agency/src/Components-Parts/Navbar.jsx
+++ AI-Agency/src/Components-Parts/Navbar.jsx
@@ -1,6 +1,8 @@
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 py-4 sticky top-0 z-20 backdrop-blur-×1 font-medium bg-white/50 dark:bg-gray-900/70"></div>
+    <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 py-4 sticky top-0 z-20 backdrop-blur-×1 font-medium bg-white/50 dark:bg-gray-900/70">
+      <img src="" />
+    </div>
   );
 };
 
