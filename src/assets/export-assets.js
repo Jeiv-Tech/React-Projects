a4aaea6e7a965bc5bd63213e7d92dc74dfc6fcaa
Jeiv-Tech <jeivjeiv85@gmail.com>
2026-09-24 15:45:21 +0800

    aaaaaaaaaaaa

--- src/assets/export-assets.js
+++ src/assets/export-assets.js
@@ -1,3 +1,54 @@
+import Facebook_icon from "./Facebook_icon.svg";
+import Instagram_icon from "./Instagram_icon.svg";
+import email_icon from "./email_icon.svg";
+import Airbnd_Logo from "./Airbnd_Logo.png";
+import Coinbase_Logo from "./Coinbase_Logo.png";
+import Microsoft_Logo from "./Microsoft_Logo.png";
+import Zoom_Logo from "./Zoom_Logo.png";
+import Google_Logo from "./Google_Logo.png";
+import Rakuten_Logo from "./Rakuten_Logo.png";
+import White_Theme_Dashboard from "./White_Theme_Dashboard.png";
+import Dark_Theme_Dashboard from "./Dark_Theme_Dashboard.png";
+import White_Theme_Icon1 from "./White_Theme_Icon1.png";
+import Dark_Theme_Icon1 from "./Dark_Theme_Icon1.png";
+import White_Theme_Icon2 from "./White_Theme_Icon2.png";
+import Dark_Theme_Icon2 from "./Dark_Theme_Icon2.png";
+import White_Theme_Icon3 from "./White_Theme_Icon3.png";
+import Dark_Theme_Icon3 from "./Dark_Theme_Icon3.png";
+import White_Theme_Icon4 from "./White_Theme_Icon4.png";
+import Dark_Theme_Icon4 from "./Dark_Theme_Icon4.png";
+import Work_Dashboard_Management from "./Work_Dashboard_Management.png";
+import Work_Fitness_App from "./Work_Fitness_App.png";
+import Work_Mobile_App from "./Work_Mobile_App.png";
+
+export const company_logo = [
+  Airbnd_Logo,
+  Coinbase_Logo,
+  Microsoft_Logo,
+  Zoom_Logo,
+  Google_Logo,
+  Rakuten_Logo,
+];
+
+export const assets = [
+  Facebook_icon,
+  Instagram_icon,
+  email_icon,
+  White_Theme_Dashboard,
+  Dark_Theme_Dashboard,
+  White_Theme_Icon1,
+  Dark_Theme_Icon1,
+  White_Theme_Icon2,
+  Dark_Theme_Icon2,
+  White_Theme_Icon3,
+  Dark_Theme_Icon3,
+  White_Theme_Icon4,
+  Dark_Theme_Icon4,
+  Work_Dashboard_Management,
+  Work_Fitness_App,
+  Work_Mobile_App,
+];
+
 // All the characters name
 export const TeamData = [
   {
